438e6e3bd46df5dd8e5ac2df6103fb9726bafee1
irinaog <goncharirina08@gmail.com>
2022-04-29 15:01:30 +0300

    create gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML("beforeend", galleryItemMarkup(galleryItems));
+
+// console.log(galleryItemMarkup(galleryItems));
+
+function galleryItemMarkup(gallery) {
+    return gallery
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+        <a href = "${original}" class = "gallery__link">
+        <img class="gallery__image" alt="${description}" src="${preview}">
+        </a>
+        </div>
+        `
+        })
+        .join('')
+};
+
+galleryEl.addEventListener('click', onItemContainerClick); 
+
+function onItemContainerClick(evt) {
+
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+
+
+};
+
+
