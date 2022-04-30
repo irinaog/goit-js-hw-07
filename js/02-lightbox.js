6739bb1c57c273f8416975ce64ccafad76be8984
irinaog <goncharirina08@gmail.com>
2022-04-30 15:09:00 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML("beforeend", galleryItemMarkup(galleryItems));
+
+function galleryItemMarkup(gallery) {
+    return gallery
+        .map(({ preview, original, description }) => {
+            return `
+        <li>
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+        </a>
+        </li>
+        `
+        })
+        .join('')
+};
+
+let lightbox = new SimpleLightbox('.gallery__item', {captionDelay: 250});
+
 
 console.log(galleryItems);
